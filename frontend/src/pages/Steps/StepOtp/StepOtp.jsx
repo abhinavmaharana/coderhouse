dfc0dc5ca8021e20dad16b6984b5c43cbc396444
abhinavmaharana <abhinavmaharana.004@gmail.com>
2023-01-08 21:03:54 +0530

    Backend started

--- frontend/src/pages/Steps/StepOtp/StepOtp.jsx
+++ frontend/src/pages/Steps/StepOtp/StepOtp.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from '../../../components/shared/Button/Button';
+import Card from '../../../components/shared/Card/Card';
+import TextInput from '../../../components/shared/TextInput/TextInput';
+import styles from './StepOtp.module.css';
 
 const StepOtp = ({ onNext }) => {
+  const [otp, setOtp] = useState('');
+  function submit() {
+
+  }
   return (
     <>
-      <div>Otp component</div>
-      <button onClick={onNext}>Next</button>
-    </>
+            <div className={styles.cardWrapper}>
+                <Card
+                    title="Enter the code we just texted you"
+                    icon="lock-emoji"
+                >
+                    <TextInput
+                        value={otp}
+                        onChange={(e) => setOtp(e.target.value)}
+                    />
+                    <div className={styles.actionButtonWrap}>
+                        <Button onClick={submit} text="Next" />
+                    </div>
+                    <p className={styles.bottomParagraph}>
+                        By entering your number, you’re agreeing to our Terms of
+                        Service and Privacy Policy. Thanks!
+                    </p>
+                </Card>
+            </div>
+        </>
   )
 }
 
