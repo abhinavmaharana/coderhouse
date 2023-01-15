0b9a3457307a41b9ab47793e0963d4e782aa6ef2
Abhinav Maharana <abhinavmaharana.004@gmail.com>
2023-01-15 18:57:32 +0530

    authentication complete

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express')
 const app = express();
-const PORT = process.env.PORT || 5500;
+const router = require('./routes')
 
+const PORT = process.env.PORT || 5500;
+app.use(express.json());
+app.use(router);
 app.get('/', (req,res) => {
     res.send('Hello from express Js');
-})
+});
 
-app.listen(PORT, () => `Listening on port ${PORT}`);
-
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
